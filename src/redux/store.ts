b4632157962ff5a315debed2e96436b2a607feff
maviifreitass <mariavitoria.freitas@yahoo.com>
2025-10-04 16:09:51 -0300

    feat: add playlist reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
+import playlistsReducer from "./playlistsSlice";
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
+        playlists: playlistsReducer,
     },
 });
 
--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,13 +1,14 @@
 import React, {type JSX} from "react";
 import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import type { RootState } from "../redux/store";
 
 interface PrivateRouteProps {
-    // TODO
     children: JSX.Element;
 }
 
 const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
-    const isAuthenticated = true; // TODO implement verification
+    const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
 
     return isAuthenticated ? children : <Navigate to="/login" replace />;
 };
